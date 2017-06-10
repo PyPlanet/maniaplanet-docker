c4d42136de78f038592cef44965c5cbd2c63e5a1
Tom Valk <tomvalk@gmail.com>
2017-06-10 15:13:35 +0200

    [FEATURE] Add docker image and settings.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Dockerized Maniaplanet Dedicated
 
+## Requirements
+
+A machine with Docker engine.
+
+## Usage
+
+### Directly from CLI
+
+todo
+
+### In docker-compose file
+
+todo
---
+++ entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# We are required to get the public ip if we don't have it in our env currently.
+if [ "$FORCE_IP_ADDRESS" = "" ]
+then
+   FORCE_IP_ADDRESS=`wget -qO- http://ifconfig.io/ip`
+fi
+if [ "$FORCE_IP_PORT" = "" ]
+then
+   FORCE_IP_PORT=${PORT-2350}
+fi
+echo "=> Going to run on forced IP: ${FORCE_IP_ADDRESS} and port: ${FORCE_IP_PORT}"
+
+# Make sure we use defaults everywhere.
+: ${TITLE:="TMStadium@nadeo"}
+: ${TITLE_PACK_URL:="https://v4.live.maniaplanet.com/ingame/public/titles/download/TMStadium@nadeo.Title.Pack.gbx"}
+: ${TITLE_PACK_FILE:=""}
+: ${DEDICATED_CFG:="config.txt"}
+: ${MATCH_SETTINGS:="MatchSettings/default.txt"}
+: ${SERVER_NAME:="My Docker Server"}
+
+# Copy the configuration files if not yet copied.
+if [ ! -f ./UserData/Config/config.txt ]; then
+    cp config.txt UserData/Config/config.txt
+fi
+if [ ! -f ./UserData/Maps/MatchSettings/default.txt ]; then
+    cp matchsettings.txt UserData/Maps/MatchSettings/default.txt
+fi
+if [ ! -f ./UserData/Maps/stadium_map.Map.gbx ]; then
+    cp stadium_map.Map.gbx UserData/Maps/stadium_map.Map.gbx
+fi
+
+# Download title.
+# TODO: Check if update is required first.
+echo "=> Downloading newest title version"
+if [ "$TITLE_PACK_FILE" = "" ]
+then
+    wget ${TITLE_PACK_URL} -qP ./UserData/Packs/
+else
+    wget ${TITLE_PACK_URL} -qO ./UserData/Packs/${TITLE_PACK_FILE}
+fi
+
+# Start dedicated.
+echo "=> Starting server, login=${LOGIN}"
+./ManiaPlanetServer $@ \
+    /nodaemon \
+    /forceip=${FORCE_IP_ADDRESS}:${FORCE_IP_PORT} \
+    /title=${TITLE} \
+    /dedicated_cfg=${DEDICATED_CFG} \
+    /game_settings=${MATCH_SETTINGS} \
+    /login=${LOGIN} \
+    /password=${PASSWORD} \
+    /servername=${SERVER_NAME} \
